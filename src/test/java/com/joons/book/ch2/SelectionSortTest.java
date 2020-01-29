8eecf7f5e2a8d568362304ea862ffc445cb9c7c2
Joon-S <niceweather94@gmail.com>
2020-01-29 23:06:05 +0900

    선형 및 이차 형태의 메소드 추가

--- src/test/java/com/joons/book/ch2/SelectionSortTest.java
+++ src/test/java/com/joons/book/ch2/SelectionSortTest.java
@@ -16,4 +16,20 @@ public class SelectionSortTest {
         assertEquals(3, a[0]);
         assertEquals(7, a[1]);
     }
+
+    @Test
+    public void linearTimeTest() {
+        int[] a = {5, 3, 1, 2, 7, 3, 6, 9};
+        int indexLowest = SelectionSort.indexLowest(a, 5);
+        assertEquals(5, indexLowest);
+        indexLowest = SelectionSort.indexLowest(a, 0);
+        assertEquals(2, indexLowest);
+    }
+
+    @Test
+    public void quadraticTimeTest() {
+        int[] a = {5, 3, 1, 2, 7, 3, 6, 9};
+        SelectionSort.selectionSort(a);
+        System.out.println(Arrays.toString(a));
+    }
 }
